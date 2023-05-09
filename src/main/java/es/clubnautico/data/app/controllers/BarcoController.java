7f77a453eaa128e85c8f435f212889fa1c2994a6
ali <wams_9@hotmail.com>
2023-05-09 11:32:35 +0200

    tercera prueba

--- src/main/java/es/clubnautico/data/app/controllers/BarcoController.java
+++ src/main/java/es/clubnautico/data/app/controllers/BarcoController.java
@@ -1,5 +1,7 @@
 package es.clubnautico.data.app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -16,5 +18,39 @@ public class BarcoController {
 	@Autowired
 	private SocioDao socioDao;
 	
-	
+	    public Socio actualizarBarcos(Long id, List<Barco> barcosActualizados) {
+	        Socio socio = socioDao.findById(id);
+	        if (socio != null) {
+	            // Actualizar barcos existentes y agregar nuevos barcos
+	            List<Barco> barcosExistentes = socio.getBarcos();
+	            for (Barco barcoActualizado : barcosActualizados) {
+	                boolean encontrado = false;
+	                for (Barco barcoExistente : barcosExistentes) {
+	                    if (barcoExistente.getId().equals(barcoActualizado.getId())) {
+	                        encontrado = true;
+	                        if (barcoActualizado.isEliminado()) {
+	                            barcoExistente.setEliminado(true);
+	                        } else {
+	                            barcoExistente.setNombre(barcoActualizado.getNombre());
+	                            barcoExistente.setDescripcion(barcoActualizado.getDescripcion());
+	                            // otros atributos
+	                        }
+	                        break;
+	                    }
+	                }
+	                if (!encontrado && !barcoActualizado.isEliminado()) {
+	                    barcoActualizado.setSocio(socio);
+	                    
+	                    barcosExistentes.add(barcoActualizado);
+	                }
+	            }
+	            socio.setBarcos(barcosExistentes);
+	            socioService.actualizarSocio(socio);
+	            return socio;
+	        } else {
+	            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Socio no encontrado");
+	        }
+	    }
+	}
+
 }
